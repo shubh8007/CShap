f728e9d34efeee8803ab093f49bbee0e86cd2b0b
shubham suryawanshi <130492776+shubh8007@users.noreply.github.com>
2023-07-22 00:00:12 +0530

    Add files via upload

---
+++ StudentManageMentSytem/StudentManagement/BOL/Student.cs
@@ -0,0 +1,31 @@
+﻿namespace BOL
+{
+    public class Student
+    {
+        public int id;
+        public string name;
+        public int age;
+        
+        public string course;
+        public double fees;
+        public DateOnly bDate;
+
+        public Student() { }
+
+        public Student(int id, string name, int age,  string course, double fees, DateOnly bDate)
+        {
+            this.id = id;
+            this.name = name;
+            this.age = age;
+            this.bDate = bDate;
+            this.course = course;
+            this.fees = fees;
+            this.bDate = bDate;
+        }
+
+        public override string ToString()
+        {
+         return   string.Format("{0},{1},{2},{3},{4},{5}",id,name,age,course,fees,bDate);
+        }
+    }
+}
---
+++ StudentManageMentSytem/StudentManagement/DAL/DBManger.cs
@@ -0,0 +1,153 @@
+﻿using BOL;
+using MySql.Data.MySqlClient;
+namespace DAL
+{
+    public class DBManger
+    {
+        public static string coString = @"server=localhost;port=3306;user=root;password=8007992007;database=dotnet";
+
+
+        public static List<Student> getAllStudents()
+        {
+            List<Student> students = new List<Student>();
+            MySqlConnection con=new MySqlConnection();
+
+            con.ConnectionString=coString;
+            string query = "select * from student";
+            try
+            {
+                MySqlCommand cmd=new MySqlCommand(query, con);
+                con.Open();
+           
+                MySqlDataReader reader=cmd.ExecuteReader();
+                while(reader.Read())
+                {
+                    int id = int.Parse(reader["id"].ToString());
+                    string name = reader["name"].ToString();
+                    int age = int.Parse(reader["age"].ToString());
+                  
+                    string course = reader["course"].ToString();
+                    double fees = double.Parse(reader["fees"].ToString());
+                    DateOnly bDate = DateOnly.Parse(reader["bDate"].ToString().Substring(0,10));
+                    Student student = new Student()
+                    {
+                        id=id,
+                        name=name,
+                        age=age,
+                     
+                        course=course,
+                        fees=fees,
+                         bDate = bDate,
+
+                    };
+                    students.Add(student);
+                }
+            }catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally { 
+                con.Close();
+            }
+            return students;
+        }
+
+        public static Student GetStudentbyId(int id)
+        {
+            Student student = null;
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString = coString;
+            
+            try
+            {
+                string query = "select * from student where id=" + id;
+                con.Open();
+                MySqlCommand cmd=new MySqlCommand(query, con);  
+                
+                MySqlDataReader reader = cmd.ExecuteReader();
+                while (reader.Read())
+                {
+                    int id1 = int.Parse(reader["id"].ToString());
+                  
+                    string name = reader["name"].ToString();
+                  
+
+                    int age = int.Parse(reader["age"].ToString());
+                   
+                    string course = reader["course"].ToString();
+                  
+                    double fees = double.Parse(reader["fees"].ToString());
+                    DateOnly bDate = DateOnly.Parse(reader["bDate"].ToString().Substring(0, 10));
+                   student = new Student()
+                    {
+                        id = id1,
+                        name = name,
+                        age = age,
+
+                        course = course,
+                        fees = fees,
+                        bDate = bDate,
+
+                    };
+
+                }
+
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            finally {
+                con.Close(); 
+            }
+
+            return student;
+        }
+
+
+        public static bool insert(Student stud)
+        { 
+            bool status = false;
+            string query = "insert into student(id,name,age,course ,fees,bDate)" +
+                "" + "values('" + stud.id + "','" + stud.name + "','" + stud.age + "','" + stud.course + "','" + stud.fees + "','" + stud.bDate +"')";
+
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString=coString;
+            try
+            {
+                con.Open();
+                MySqlCommand cmd=new MySqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                status = true;
+            }catch(Exception ex) {
+                Console.WriteLine(ex.Message);
+                    }
+            return status;
+        }
+
+        public static bool deleteById(int id)
+        
+        {
+            bool status = false;
+         
+            MySqlConnection con = new MySqlConnection();
+            con.ConnectionString=coString;
+
+            try
+            {
+                string query = "delete from student where id=" + id;
+                con.Open();
+                MySqlCommand cmd = new MySqlCommand(query, con);
+                cmd.ExecuteNonQuery();
+                status = true;
+            }catch(Exception e)
+            {
+                Console.WriteLine (e.Message);
+            }
+            finally { 
+                con.Close();
+            }    
+            return false;
+        }
+    }
+}

06c5a501cb38000cbc4a0bbef2b1ac218f17232d
shubham suryawanshi <130492776+shubh8007@users.noreply.github.com>
2023-07-22 22:41:11 +0530

    Add files via upload

---
+++ StudentManageMentSytem/StudentManagement/StudentApp1/Controllers/ValuesController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BOL;
+using DAL;
+using BLL;
+namespace StudentApp1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+        [HttpGet]
+        public List<Student>  Getstudent()
+        {
+            List<Student> list = new List<Student>();
+
+            list = Bll.displayStudent();
+            foreach (Student student in list)
+            {
+                Console.WriteLine(student);
+            }
+            return list;
+           
+        }
+    }
+}
